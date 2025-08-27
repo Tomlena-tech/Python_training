b4fc036e960a6f22981f7068ad9a4576c8fd05ba
Tomlena-tech <decourttthomas@gmail.com>
2025-08-27 16:33:39 +0200

    test Python

---
+++ exemple.py
@@ -0,0 +1,8 @@
+#codage:utf-8
+age_de_la_personne = 14 
+print(type(age_de_la_personne))
+biere_de_dorian = 7
+prix_de_la_girafe = 20
+texte = " Le prix de la la bière de Dorian est de {} euros et le prix de la girafe est de {} euros" 
+
+print(texte.format(biere_de_dorian, prix_de_la_girafe))
